d70117b0cc778cc73c3d1094bf0729b59b1292c7
Kush Patel <ktpatel3@myseneca.ca>
2022-10-08 18:44:26 -0400

    updated server.js

--- data-service.js
+++ data-service.js
@@ -1,20 +1,30 @@
-const fs = require('node:fs');
+/*************************************************************************
+* BTI325– Assignment 2
+* I took the help of my classmate in making the dataservice file whose name is ujval and also one of my friend Kalpaj help me a lot 
+in making thee server.js file actually he already complete this semester before from another college so he is helping me in making 
+this assignment. 
+*
+* Name: Kush Patel Student ID: 104006218 Date: 5th october, 2022
+*_
+* Your app’s URL (from Cyclic) : ______________________________________________
+*
+*************************************************************************/ 
+
+
+
+const fs = require('fs');
 let employees = [];
 let departments = [];
 
 module.exports.initialize = function () {
   return new Promise((resolve, reject) => {
     fs.readFile('./data/employees.json', (err, data) => {
-      if (err) {
-        reject(err);
-      }
+      if (err) reject("Failure to read file employees.json!");
       employees = JSON.parse(data);
       resolve();
     });
     fs.readFile('./data/departments.json', (err, data) => {
-      if (err) {
-        reject(err);
-      }
+      if (err) reject("Failure to read file departments.json!");     
       departments = JSON.parse(data);
       resolve();
     });
@@ -24,7 +34,7 @@ module.exports.initialize = function () {
 module.exports.getAllEmployees = function () {
   return new Promise((resolve, reject) => {
     if (employees.length == 0) {
-      reject('No Employee Found!');
+      reject('no results returned');
     }
     resolve(employees);
   });
@@ -32,23 +42,23 @@ module.exports.getAllEmployees = function () {
 
 module.exports.getManagers = function () {
   return new Promise((resolve, reject) => {
-    var managers = [];
+    var man = [];
     for (let i = 0; i < employees.length; i++) {
       if (employees[i].isManager == true) {
-        managers.push(employees[i]);
+        man.push(employees[i]);
       }
     }
-    if (managers.length == 0) {
-      reject('No Managers Found!');
+    if (man.length == 0) {
+      reject('no results returned');
     }
-    resolve(employees);
+    resolve(man);
   });
 };
 
 module.exports.getDepartments = function () {
   return new Promise((resolve, reject) => {
     if (departments.length == 0) {
-      reject('No Department Found!');
+      reject('no results returned');
     }
     resolve(departments);
   });
--- server.js
+++ server.js
@@ -1,55 +1,61 @@
-const express = require('express');
-const path = require('path');
-const app = express()
-app.use(express.static("public"))
-var dataservice = require('./data-service.js');
+/*************************************************************************
+* BTI325– Assignment 2
+* I took the help of my classmate in making the dataservice file whose name is ujval and also one of my friend Kalpaj help me a lot 
+in making thee server.js file actually he already complete this semester before from another college so he is helping me in making 
+this assignment. 
+*
+* Name: Kush Patel Student ID: 104006218 Date: 5th october, 2022
+*_
+* Your app’s URL (from Cyclic) : ______________________________________________
+*
+*************************************************************************/ 
 
+var express = require("express");
+var path = require("path");
+const app = express();
+app.use(express.static("public"));
+var dataservice = require('./data-service.js');
 const port = process.env.PORT || 8080;
 function onhttp(){
-  console.log("Express http server listening on port",port );
+  console.log("Express http server listening on port",port);
 }
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname,"./views/home.html"));
-})
+});
 
 app.get("/about", (req, res) => {
     res.sendFile(path.join(__dirname,"./views/about.html"));
-})
+});
 
 app.get('/employees', function (req, res) {
-  dataService.getAllEmployees().then((data) => {
+  dataservice.getAllEmployees().then((data) => {
     res.json(data);
   });
 });
 
 app.get("/managers", function(req, res){
-  dataservice.getAllManagers().then((data) => {
+  dataservice.getManagers().then((data) => {
     res.json(data);
   }
 )});
 
 
-app.get("/department", function(req, res){
-  dataservice.getAllDepartments().then((data) => {
+app.get("/departments", function(req, res){
+  dataservice.getDepartments().then((data) => {
     res.json(data);
   }
 )});
+
 app.use(function (req, res) {
-  res.status(404).send('Page Not Found');
+  res.sendFile(path.join(__dirname,"./views/error.html"));
 });
 
-
-
-//// setup http server to listen on HTTP_PORT
-dataservice
- .initialize()
- .then(function () {
-   app.listen(HTTP_PORT, onHTTPStart);
+dataservice.initialize().then(function () {
+  app.listen(port, onhttp);
  })
  .catch(function (err) {
    console.log('Failed to start!' + err);
  });
 
 
-app.listen(port, onhttp);
