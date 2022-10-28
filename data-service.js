5bf915e3ea9748ea7e8d9d666e6e0ff5d0dc54ac
Kush Patel <ktpatel3@myseneca.ca>
2022-10-28 18:41:43 -0400

    update server.js

--- data-service.js
+++ data-service.js
@@ -63,3 +63,82 @@ module.exports.getDepartments = function () {
     resolve(departments);
   });
 };
+
+/*------------------------------------------------------------------------------------------------------------------- */
+/* Assignment 2 */
+
+
+// part 3 step 3
+module.exports.addEmployee = (employeeData)=> {return new Promise((resolve, reject)=> {
+    if (employeeData.isManager == undefined) {
+      employeeData.isManager = false; // set it to false
+    } else{
+      employeeData.isManager = true; // se it to true
+    }
+    employeeData.employeeNum = employees.length+1; // set the employeeNum property of employeeData to be the length of the "employees" array plus one (1)
+    employees.push(employeeData); // push the updated employeedata object onto the "employees" array
+    resolve(employees);
+  });
+}
+
+// PART 4
+// for employee by status.
+module.exports.getEmployeesByStatus = (status)=> {
+  return new Promise((resolve, reject)=> {
+    var emp_info = [];
+    for (let i= 0; i < employees.length; i++) {
+      if (employees[i].status == status) {  // compare for the same status.
+        emp_info.push(employees[i]);   // if it is same then it will promt that to user screen.
+      }
+    }
+  if (employees.length == 0) {   // if there is no employee it shows no result returned.
+    reject("no result returnned");
+  }
+  resolve(emp_info);
+  });
+}
+// for employee by department 
+module.exports.getEmployeesByDepartment = (department)=> {
+  return new Promise((resolve, reject)=> {
+    var dep_info = [];
+    for (let i= 0; i < employees.length; i++) {
+      if (employees[i].department == department) { // compare for the same department.
+        dep_info.push(employees[i]);// if it founds then it will show the user.
+      }
+    }
+  if (employees.length == 0) {  // if there is not employee found by departement then it shows no result returned.
+    reject("no result returnned");
+  }
+  resolve(dep_info);
+  });
+}
+// for employee by manager.
+module.exports.getEmployeesByManager = (manager)=> {
+  return new Promise((resolve, reject)=> {
+    var manager_info = [];
+    for (let i= 0; i < employees.length; i++) {
+      if (employees[i].employeeManagerNum == manager) {// compare for the same manager.
+        manager_info.push(employees[i]);// if it founds then it will show the user.
+      }
+    }
+  if (employees.length == 0) {// if there is not employee found by manager then it shows no result returned.
+    reject("no result returnned");
+  }
+  resolve(manager_info); 
+  });
+}
+// for employee by number.
+module.exports.getEmployeeByNum = (numb) =>{
+  return new Promise((resolve, reject)=> {
+    var num_info = [];
+    for (let i= 0; i < employees.length; i++) {
+      if (employees[i].employeeNum == numb) {// compare for the same number.
+        num_info.push(employees[i]);// if it founds then it will show the user.
+      }
+    }
+  if (employees.length == 0) {// if there is not employee found by number then it shows no result returned.
+    reject("no result returnned");
+  }
+  resolve(num_info);
+  });
+}
--- server.js
+++ server.js
@@ -15,10 +15,86 @@ var path = require("path");
 const app = express();
 app.use(express.static("public"));
 var dataservice = require('./data-service.js');
+// part 2 step 1
+var multer = require("multer");
+//part 2 step 3
+const fs = require('fs');
+// part 3 step 1
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
 const port = process.env.PORT || 8080;
 function onhttp(){
   console.log("Express http server listening on port",port);
 }
+// part 2 step 1
+
+const storage = multer.diskStorage({
+  destination: "./public/images/uploaded",
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  }
+});
+const upload = multer({ storage: storage });
+
+//part 2 step 2
+app.post("/images/add", upload.single("imageFile"), (req, res) => {
+  res.redirect("/images");
+});
+
+//part 2 step 3
+app.get("/images",function(req,res){
+  fs.readdir("./public/images/uploaded", function(err, items){
+      for (var i=0; i<items.length; i++) {  // for loop so that i can print every image that user can upload.
+          items[i];
+      }
+      return res.json({images: items});
+    }) 
+});
+
+// part 3 step 2
+app.post("/employees/add", (req, res) => {
+  dataservice.addEmployee(req.body).then((data) =>{
+      res.redirect("/employees"); // When the addEmployee function resolves successfully, redirect to the "/employees" route. 
+  });
+})
+
+// part 4 step 1
+app.get('/employees', function(req, res) {
+if(req.query.department){
+  dataservice.getEmployeesByDepartment(req.query.department).then((data) => { // this checks if the query string is for department. 
+    res.json(data);
+});
+} else if(req.query.status){
+  dataservice.getEmployeesByStatus(req.query.status).then((data) => { // this checks if the query string is for status.
+    res.json(data);
+});
+} else if(req.query.manager){ 
+  dataservice.getEmployeesByManager(req.query.manager).then((data) => { // this checks if the query string is for manager.
+    res.json(data);
+});
+} else if(req.query.employeeNum){ 
+  dataservice.getEmployeeByNum(req.query.employeeNum).then((data) => { // this checks if the query string is for emp number.
+    res.json(data);
+  });
+}
+else{ 
+  dataservice.getAllEmployees().then((data) => {
+    res.json(data);
+});
+}
+});
+
+
+// part 4 step 2
+
+app.get("/employee/:employeNum",(req,res)=>{ //This route takes one parameter which means employeeNum. 
+  dataservice.getEmployeeByNum(req.params.employeNum).then((data)=>{ //return a JSON formatted string containing the employee (one object) whose employeeNum matches the value.
+    res.json(data);
+      });
+    });
+
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname,"./views/home.html"));
@@ -26,6 +102,16 @@ app.get("/", (req, res) => {
 
 app.get("/about", (req, res) => {
     res.sendFile(path.join(__dirname,"./views/about.html"));
+});
+
+// Step 2
+
+app.get('/employees/add', function(req, res) {
+  res.sendFile(path.join(__dirname, "./views/addEmployee.html"));
+});
+
+app.get('/images/add', function(req, res) {
+  res.sendFile(path.join(__dirname, './views/addImage.html'));
 });
 
 app.get('/employees', function (req, res) {
@@ -50,6 +136,7 @@ app.get("/departments", function(req, res){
 app.use(function (req, res) {
   res.sendFile(path.join(__dirname,"./views/error.html"));
 });
+
 
 dataservice.initialize().then(function () {
   app.listen(port, onhttp);
